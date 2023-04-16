892793a28e7ef5b9fadd75fef5408207ac0c0968
Wojciech Jedynak <wjedynak@gmail.com>
2023-04-16 22:00:10 +0900

    123 (#1)
    * 123
    
    * fix build
    
    * fix test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn get_greeting() -> String {
+    "Hello, World!".to_owned()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_greeting());
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_greeting() {
+        assert_eq!(get_greeting(), "Hello, World!".to_owned());
+    }
 }
